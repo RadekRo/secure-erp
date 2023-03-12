06f4557037757ecc8a0d4a825730022b5ad47d93
RadekRo <r.roclawski@ankara.pl>
2023-03-12 19:28:43 +0100

    Added sales files changed by Adrian Paszek

--- model/sales/sales.py
+++ model/sales/sales.py
@@ -12,3 +12,12 @@ from model import data_manager, util
 
 DATAFILE = "model/sales/sales.csv"
 HEADERS = ["Id", "Customer", "Product", "Price", "Date"]
+
+def load_data(header = 0):    
+    transaction_list = data_manager.read_table_from_file(DATAFILE)
+    header == 1 and transaction_list.insert(0, HEADERS)
+    return transaction_list
+
+def save_data(incoming_data):
+    incoming_data.remove(incoming_data[0])
+    data_manager.write_table_to_file(DATAFILE, incoming_data)
